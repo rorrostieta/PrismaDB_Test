08d3e42a657056a619e8e4486980b9153f2c891a
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-05-03 21:19:34 -0500

    creating seeds for students

--- prisma/seed.js
+++ prisma/seed.js
@@ -63,7 +63,56 @@ const prisma = new PrismaClient();
       },
     });
 
+    const student1 = await prisma.student.upsert({
+      where: { name: 'Student1' },
+      update: {},
+      create: {
+        name: 'Student1',
+				lang: 'Javascript',
+				missionCommander: 'Carlo',
+        enrollments: 4,
+        hasCertification: true
+      },
+    });
+
+    const student2 = await prisma.student.upsert({
+      where: { name: 'Student2' },
+      update: {},
+      create: {
+        name: 'Student2',
+				lang: 'Javascript',
+				missionCommander: 'Fernanda',
+        enrollments: 4,
+        hasCertification: false
+      },
+    });
+
+    const student3 = await prisma.student.upsert({
+      where: { name: 'Student3' },
+      update: {},
+      create: {
+        name: 'Student3',
+				lang: 'Javascript',
+				missionCommander: 'Fernanda',
+        enrollments: 6,
+        hasCertification: true
+      },
+    });
+
+    const student4 = await prisma.student.upsert({
+      where: { name: 'Student4' },
+      update: {},
+      create: {
+        name: 'Student4',
+				lang: 'Javascript',
+				missionCommander: 'Carlo',
+        enrollments: 5,
+        hasCertification: true
+      },
+    });
+
     console.log('Create 5 explorers');
+    console.log('Create 4 students')
   } catch(e) {
     console.error(e);
     process.exit(1);
